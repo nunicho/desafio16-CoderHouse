2cac2169ac09c3a5317cf2900b4d11a2d21c4bc4
MauricioAlonso <program.alonso@gmail.com>
2023-12-08 23:13:20 -0300

    arrancando el desafio

---
+++ src/dao/DB/models/carritos.modelo.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Producto = require("../models/productos.modelo.js");
+
+const carritoSchema = new mongoose.Schema({
+  productos: {
+    type: [
+      {
+        producto: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: Producto,
+        },
+        cantidad: Number,
+      },
+    ],
+  },
+  amount: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const carritoModelo = mongoose.model("carritos", carritoSchema);
+
+module.exports = carritoModelo;
+
+/*
+const mongoose = require("mongoose");
+const Producto = require("../models/productos.modelo.js");
+
+const carritoSchema = new mongoose.Schema({
+  productos: {
+    type: [
+      {
+        producto: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: Producto,
+        },
+        cantidad: Number, // Agrega el campo cantidad
+      },
+    ],
+  },
+});
+
+const carritoModelo = mongoose.model("carritos", carritoSchema);
+
+module.exports = carritoModelo;
+
+*/
