758e10b49990b619ad38de7d3e408127ecd10b62
MauricioAlonso <program.alonso@gmail.com>
2023-12-12 00:33:29 -0300

    listo swagger de crear producto

--- src/router/products.router.js
+++ src/router/products.router.js
@@ -33,59 +33,23 @@ router.get("/:id", productosController.obtenerProducto, (req, res) => {
     }
     res.header("Content-type", "application/json");
     res.status(200).json({ productoDB });
-    // req.logger.info(
-    //   `Acceso exitoso a detalle producto Id: ${productoDB._id} - Nombre: ${productoDB.title}`
-    // );
   } catch (error) {
     res.status(500).json({ error: "Error interno del servidor" });
-    // req.logger.error(
-    //   `Error al abrir detalle de producto - Detalle: ${error.message}`
-    // );
+    
   }
 });
-
-// router.get("/:id", async (req, res) => {
-//   let id = req.params.id;
-
-//   if (!mongoose.Types.ObjectId.isValid(id))
-//     return res.status(400).json({ error: "id inválido" });
-
-//   let productoDB = await productosModelo.findById(id);
-
-//   if (!productoDB)
-//     return res.status(404).json({ error: `Producto con id ${id} inexistente` });
-
-//   res.status(200).json({ productoDB });
-// });
 
 
 //------------------------------------------------------------------------ PETICION POST
 
 router.post("/", async (req, res) => {
-  let producto = req.body;
-  if (
-    !producto.title ||
-    !producto.description ||
-    !producto.price ||
-    !producto.thumbnail ||
-    !producto.code ||
-    !producto.stock
-  )
-    return res.status(400).json({ error: "Faltan datos" });
-
-  let existe = await productosModelo.findOne({ code: producto.code });
-  if (existe)
-    return res.status(400).json({
-      error: `El código ${producto.code} ya está siendo usado por otro producto.`,
-    });
-
   try {
-    let productoInsertado = await productosModelo.create(producto);
-    res.status(201).json({ productoInsertado });
+    await productosController.crearProducto(req, res);
   } catch (error) {
     res.status(500).json({ error: "Error inesperado", detalle: error.message });
   }
 });
+
 
 //------------------------------------------------------------------------ PETICION PUT
 
